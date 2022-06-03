abec82e48c45885841f8494d5f5038d160b242ea
Yurii Osipchuk <yura.osipchyk@gmail.com>
2022-06-03 19:52:14 +0300

    Implemented task

--- src/main.go
+++ src/main.go
@@ -2,4 +2,33 @@ package main
 
 import "fmt"
 
-func main() {}
+func main() {
+	var number int
+	var guessNumber int
+
+	guessNumber = 51
+
+	// fmt.Printf("guessNumber = %v\n", guessNumber)
+	// fmt.Printf("number = %v\n", number)
+
+	for number != guessNumber {
+		fmt.Print("Enter number: ")
+		fmt.Scan(&number)
+		
+		if number >= 1 && number <= 100 {
+			if number < guessNumber{
+				fmt.Println("Number is small")
+			}
+
+			if number > guessNumber{
+				fmt.Println("Number is big")
+			}
+
+			if number == guessNumber{
+				fmt.Println("Success")
+			}
+		} else {
+			fmt.Println("Number incorrect")
+		}
+	}
+}
